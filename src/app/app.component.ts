88833f59e86e6bce000017e1d5a14326456324cc
Edson Sousa <edson3711.es@gmail.com>
2024-12-10 08:21:50 -0300

    cria componente de home e ajusta big-card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MenuBarComponent } from "./components/menu-bar/menu-bar.component";
 import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MenuBarComponent, MenuTitleComponent],
+  imports: [CommonModule, RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
