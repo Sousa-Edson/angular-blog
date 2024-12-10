88f880972a71b744bfd9e374e5fc0dc1122f71f8
Edson Sousa <edson3711.es@gmail.com>
2024-12-10 14:07:48 -0300

    ajusta a parte dinamicamica do componente

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,19 +3,23 @@ import { BigCardComponent } from '../../components/big-card/big-card.component';
 import { MenuTitleComponent } from '../../components/menu-title/menu-title.component';
 import { SmallCardComponent } from "../../components/small-card/small-card.component";
 import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
+import { MOCK } from '../../data/mock'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
+    CommonModule,
     MenuTitleComponent,
     BigCardComponent,
     SmallCardComponent,
     MenuBarComponent
-],
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  dados = MOCK;
+  itemComId1 = this.dados.find(item => item.id === '1');
 }
